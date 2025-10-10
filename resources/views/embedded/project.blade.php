f9ce029eb45b306cba7d5563713d6e5852668f52
Martin Erren <martin3r@Mac.fritz.box>
2025-10-10 20:07:17 +0200

    policies final

--- resources/views/embedded/project.blade.php
+++ resources/views/embedded/project.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
     <div class="h-full">
+        {{-- DEBUG: Test ob embedded Komponente geladen wird --}}
+        <div class="bg-red-100 p-4 mb-4">
+            <p class="text-red-800">DEBUG: Embedded Project View geladen</p>
+            <p class="text-red-800">Projekt: {{ $plannerProject->name }}</p>
+            <p class="text-red-800">Zeit: {{ now() }}</p>
+        </div>
+        
         {{-- Embedded: embedded Projekt-Komponente rendern --}}
         @livewire('planner.embedded.project', ['plannerProject' => $plannerProject])
     </div>
