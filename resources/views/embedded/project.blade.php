a9e07a18c8124e3866489fa1ab3b78ff4ad35871
Martin Erren <martin3r@Mac.fritz.box>
2025-10-10 19:44:21 +0200

    policies final

--- resources/views/embedded/project.blade.php
+++ resources/views/embedded/project.blade.php
@@ -2,18 +2,8 @@
 
 @section('content')
     <div class="h-full">
-        {{-- Embedded: bestehende Projekt-Komponente rendern --}}
-        @livewire('planner.project', ['plannerProject' => $plannerProject])
-        <script>
-            // Nach dem Rendern alle Task-Karten-Links auf embedded umbiegen
-            document.addEventListener('DOMContentLoaded', function(){
-                document.querySelectorAll('a[wire\\:navigate][href*="/planner/tasks/"]').forEach(function(a){
-                    const id = a.getAttribute('href').split('/').pop();
-                    a.removeAttribute('wire:navigate');
-                    a.setAttribute('href', '/planner/embedded/planner/tasks/' + encodeURIComponent(id));
-                });
-            });
-        </script>
+        {{-- Embedded: embedded Projekt-Komponente rendern --}}
+        @livewire('planner.embedded.project', ['plannerProject' => $plannerProject])
     </div>
 @endsection
 
