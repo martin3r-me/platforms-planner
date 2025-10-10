53d657b93d3c07769abea4bcfff31810a06f337c
Martin Erren <martin3r@Mac.fritz.box>
2025-10-10 11:04:52 +0200

    policies final

--- resources/views/embedded/project.blade.php
+++ resources/views/embedded/project.blade.php
@@ -1,7 +1,7 @@
 @extends('platform::layouts.embedded')
 
 @section('content')
-    <div class="min-h-screen">
+    <div class="h-full">
         @livewire('planner.project', ['plannerProject' => $plannerProject])
     </div>
 @endsection
