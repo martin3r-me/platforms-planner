602fb90387345493916055b3274e276b89eee448
Martin Erren <martin3r@Mac.fritz.box>
2025-11-06 20:23:58 +0100

    sidebar

--- resources/views/livewire/task.blade.php
+++ resources/views/livewire/task.blade.php
@@ -422,7 +422,7 @@
                     wire:click="saveDueDate"
                     wire:loading.attr="disabled"
                     wire:target="saveDueDate"
-                    :disabled="!$selectedDate"
+                    wire:disabled="!selectedDate"
                     class="inline-flex items-center gap-2 px-4 py-2 text-sm font-medium rounded-lg bg-[var(--ui-primary)] text-white hover:bg-[var(--ui-primary-80)] focus:outline-none focus:ring-2 focus:ring-[var(--ui-primary)]/20 disabled:opacity-50 disabled:cursor-not-allowed transition-all duration-200"
                 >
                     <span wire:loading.remove wire:target="saveDueDate" class="inline-flex items-center gap-2">
